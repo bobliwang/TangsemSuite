71d77f4b563c8a18ba11992cf9dbb62d7b972893
bobliwang <bobliwang@gmail.com>
2012-09-10 17:21:25 +1000

    Generation for ViewModel/Entity Mapping.

---
+++ Tangsem.Suite/tests/web/Tangsem.Generator.WebMvc3Demo/ViewModels/ViewModelMappings.Designer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Reflection;
+
+public static class ViewModelMappingsExt
+{
+  public static Tangsem.Generator.WebMvc3Demo.Common.Domain.Entities.State ToState(this Tangsem.Generator.WebMvc3Demo.ViewModels.StateViewModel stateViewModel)
+  {
+    var entity = new Tangsem.Generator.WebMvc3Demo.Common.Domain.Entities.State();
+    
+    stateViewModel.MapToState(entity);
+
+    return entity;
+  }
+
+  public static void MapToState(this Tangsem.Generator.WebMvc3Demo.ViewModels.StateViewModel stateViewModel, Tangsem.Generator.WebMvc3Demo.Common.Domain.Entities.State entity)
+  { 
+    entity.Id = stateViewModel.Id;
+    entity.Name = stateViewModel.Name;
+    entity.CreatedById = stateViewModel.CreatedById;
+  }
+}
