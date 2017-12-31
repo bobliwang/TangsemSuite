823bbdc7f7c709f2955e12abac3a98aa35948035
Li Wang <Li.Wang@Nintex.com>
2017-12-31 23:56:23 +1100

    Add TMobility config.

--- Tangsem.Suite/Tangsem.Generator.Console/Program.cs
+++ Tangsem.Suite/Tangsem.Generator.Console/Program.cs
@@ -12,7 +12,7 @@ namespace Tangsem.Generator.Console
   {
     static void Main(string[] args)
     {
-        var path = @"O365Tasks.xml";
+        var path = @"TMobility-Express.xml";
       //var path = @"MTA.xml";
 
       if (args != null && args.Length > 1)
