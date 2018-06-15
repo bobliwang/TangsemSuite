0f868f8f95b02ce338a7707b97feb3e69c5e70b5
Li Wang <bobliwang@gmail.com>
2018-06-15 23:32:17 +1000

    ok

---
+++ Tangsem.Suite/projects/Tangsem.NHibernate/Extenstions/ExtendedLinqToHqlGeneratorsRegistry.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate.Linq.Functions;
+
+namespace Tangsem.NHibernate.Extenstions
+{
+  public class ExtendedLinqToHqlGeneratorsRegistry : DefaultLinqToHqlGeneratorsRegistry
+  {
+    public ExtendedLinqToHqlGeneratorsRegistry()
+    {
+      this.Merge(new JsonArrayLengthGenerator());
+
+      this.Merge(new IsLikeGenerator());
+      this.Merge(new IsNulOrEmptyGenerator());
+      this.Merge(new IsNulOrWhitespaceGenerator());
+    }
+  }
+}
---
+++ Tangsem.Suite/projects/Tangsem.NHibernate/Extenstions/IsNulOrEmptyGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using NHibernate.Hql.Ast;
+using NHibernate.Linq.Functions;
+using NHibernate.Linq.Visitors;
+using NHibernate.Util;
+
+namespace Tangsem.NHibernate.Extenstions
+{
+  public class IsNulOrEmptyGenerator : BaseHqlGeneratorForMethod
+  {
+    public IsNulOrEmptyGenerator()
+    {
+      this.SupportedMethods = new[]
+      {
+        ReflectHelper.GetMethod(() => string.IsNullOrEmpty(null))
+      };
+    }
+
+    public override HqlTreeNode BuildHql(MethodInfo method, Expression targetObject,
+      ReadOnlyCollection<Expression> arguments, HqlTreeBuilder treeBuilder, IHqlExpressionVisitor visitor)
+    {
+      var expr1 = treeBuilder.IsNull(visitor.Visit(arguments[0]).AsExpression());
+      var expr2 = treeBuilder.Equality(visitor.Visit(arguments[0]).AsExpression(), treeBuilder.Constant(""));
+
+
+      return treeBuilder.BooleanOr(expr1, expr2);
+    }
+  }
+}
