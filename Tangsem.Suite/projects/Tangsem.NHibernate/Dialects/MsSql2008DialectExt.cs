5459214a5b7270d2e18346f87736da274a6d4b11
Li Wang <bobliwang@gmail.com>
2018-07-25 22:25:18 +1000

    Tidy up a bit

---
+++ Tangsem.Suite/projects/Tangsem.NHibernate/Dialects/MsSql2008DialectExt.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+using NHibernate.Dialect;
+using NHibernate.Dialect.Function;
+
+namespace Tangsem.NHibernate.Dialects
+{
+    public class MsSql2008DialectExt : MsSql2008Dialect
+    {
+        public MsSql2008DialectExt()
+        {
+            this.RegisterFunction("dbo.fn_GetJsonArrayLength", new StandardSQLFunction("dbo.fn_GetJsonArrayLength", NHibernateUtil.Int32));
+        }
+    }
+}
